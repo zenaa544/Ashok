4cee42ae7e4eec313cc4fef7a46d35f2898eb5c2
Ashok Kumar Jena <79446762+zenaa544@users.noreply.github.com>
2024-09-28 10:55:52 +0530

    Add files via upload

---
+++ Design Patterns Examples C++/Singleton Logger/Logger.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+# include "Logger.hpp"
+
+using namespace std;
+
+Logger* Logger::LoggerInstance = nullptr;
+mutex Logger::loggerMutex;
+int Logger::ctr = 0;
+
+Logger::Logger() {
+    ctr++;   
+    cout << "Number of Logger instance  created " << ctr << endl;
+}
+
+Logger* Logger::getLogger() {
+    if (LoggerInstance == nullptr) {
+        //lock_guard<mutex> lock(loggerMutex);
+        loggerMutex.lock()
+        if (LoggerInstance == nullptr) {
+            LoggerInstance = new Logger();
+        }
+    }
+    return LoggerInstance;
+}
+
+Logger::log(string message) {
+    cout << message << endl;
+}
---
+++ Design Patterns Examples C++/Singleton Logger/Logger.hpp
@@ -0,0 +1,20 @@
+#ifndef logger_h
+#define logger_h
+
+#include <string>
+#include <mutex>
+using namespace std;
+
+class Logger {
+    Logger();//private constructor for singleton
+    static Logger* LoggerInstance;
+    static mutex loggerMutex;
+    static int ctr;
+    Logger * Logger(const Logger&); //private copy constructor
+    Logger& operator=(const Logger&); //private assignment operator
+ public:
+    static Logger* getLogger();
+    void log(string message);
+}   
+
+#endif
