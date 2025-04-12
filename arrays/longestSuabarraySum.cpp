69d7382f3036ade39d9bc516d658e48b9b0f7331
Ashok Kumar Jena <79446762+zenaa544@users.noreply.github.com>
2025-04-12 13:43:39 +0530

    Create longestSuabarraySum.cpp

---
+++ arrays/longestSuabarraySum.cpp
@@ -0,0 +1,30 @@
+//Find length of longest subarray - Prefixsum method, Used when array conatins _ve,0s and -ve
+//Use 2 pointer for only +vesand 0s
+
+int getLongestSubarray(vector<int>& nums, int k){
+    unordered_map<int, int> prefixSum;
+    int length = 0;
+    int maxLength = 0;
+    long long sum = 0;
+
+    for (int i = 0; i < nums.size(); i++) {
+        sum += nums[i];
+
+        if (sum == k) {
+            length = i + 1;
+            maxLength = max(length, maxLength);
+        }
+
+        if (prefixSum.find(sum - k) != prefixSum.end()) {
+            length = i - prefixSum[sum - k];
+            maxLength = max(length, maxLength);
+        }
+
+        // Only store the first occurrence of each sum
+        if (prefixSum.find(sum) == prefixSum.end()) {
+            prefixSum[sum] = i;
+        }
+    }
+
+    return maxLength;
+}
