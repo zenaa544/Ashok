91dc84d81c2e8d3b8bee4f2ac939d2239edb4028
Ashok Kumar Jena <79446762+zenaa544@users.noreply.github.com>
2025-04-17 12:27:26 +0530

    Create ReverseLinkedListinKGroups.cpp

---
+++ LinkedList/ReverseLinkedListinKGroups.cpp
@@ -0,0 +1,46 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    // Reverses first k nodes starting from head, sets nextGroupHead to the next node after reversed group
+    ListNode* reverseFirstK(ListNode* head, int k, ListNode*& nextGroupHead) {
+        if (k == 1) {
+            nextGroupHead = head->next;
+            return head;
+        }
+
+        ListNode* newHead = reverseFirstK(head->next, k - 1, nextGroupHead);
+        head->next->next = head;
+        head->next = nullptr;
+        return newHead;
+    }
+
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        // Count if there are at least k nodes left to reverse
+        ListNode* temp = head;
+        int count = 0;
+        while (temp && count < k) {
+            count++;
+            temp = temp->next;
+        }
+
+        if (count < k) return head; // Not enough nodes to reverse
+
+        // Reverse first k nodes
+        ListNode* nextGroupHead = nullptr;
+        ListNode* newHead = reverseFirstK(head, k, nextGroupHead);
+
+        // head is now the tail of reversed group
+        head->next = reverseKGroup(nextGroupHead, k);
+
+        return newHead;
+    }
+};
