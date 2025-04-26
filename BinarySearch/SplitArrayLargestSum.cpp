8f6f12769d5ff964697069026b0e335203ba2183
Ashok Kumar Jena <79446762+zenaa544@users.noreply.github.com>
2025-04-26 20:31:07 +0530

    Update SplitArrayLargestSum.cpp

--- BinarySearch/SplitArrayLargestSum.cpp
+++ BinarySearch/SplitArrayLargestSum.cpp
@@ -1,3 +1,9 @@
+Given an integer array nums and an integer k, split nums into k non-empty subarrays such that the largest sum of any subarray is minimized.
+
+Return the minimized largest sum of the split.
+
+A subarray is a contiguous part of the array.
+
 bool isPossibleToPartition(vector<int>& arr, int maxPartitions, int partitionSumLimit){
 
  int numPartitions=1;
