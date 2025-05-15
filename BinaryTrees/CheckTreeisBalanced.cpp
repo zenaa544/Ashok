f363e71e6701f1fae6876b443a56b62d0c3e0112
Coding Practice <144140790+letslearntocodebetter@users.noreply.github.com>
2025-05-15 18:39:26 +0200

    Add alternate method of isBalanced

--- BinaryTrees/CheckTreeisBalanced.cpp
+++ BinaryTrees/CheckTreeisBalanced.cpp
@@ -10,11 +10,43 @@ int height(TreeNode<int>* root){
     return max(leftHeight,rightHeight)+1;
 
 }
+
 bool isBalancedBT(TreeNode<int>* root){
     // Write your code here.
+    
 
     if(root==NULL)return true;
 
     int treeHeight=height(root);
+    
     if(treeHeight==-1)return false;
     else return true;
+}
+
+int checkHeight(TreeNode *root, bool &isBalanced) 
+{
+    if (root == nullptr || !isBalanced)
+        return 0;
+
+    int left = checkHeight(root->left, isBalanced);
+    int right = checkHeight(root->right, isBalanced);      
+    if (std::abs(leftHeight - rightHeight) > 1) {
+            isBalanced = false;
+            return 0; // No need to calculate further
+    }
+
+    // pass the height to the parent.
+    return max (left, right) + 1;
+}
+
+
+
+bool isBalancedTree(TreeNode* root) 
+{
+    bool isBalanced = true;
+
+    checkHeight(root, isBalanced);
+
+    return isBalalnced;
+    
+}
