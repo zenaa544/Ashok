764269afdba911fe0c1983b3d1ab6d22df598fdf
Ashok Kumar Jena <79446762+zenaa544@users.noreply.github.com>
2025-06-12 10:59:52 +0530

    Create LRUCache.cpp

---
+++ Design/LRUCache.cpp
@@ -0,0 +1,96 @@
+Design a data structure that follows the constraints of a Least Recently Used (LRU) cache.
+
+Implement the LRUCache class:
+
+LRUCache(int capacity) Initialize the LRU cache with positive size capacity.
+int get(int key) Return the value of the key if the key exists, otherwise return -1.
+void put(int key, int value) Update the value of the key if the key exists. Otherwise, add the key-value pair to the cache.
+  If the number of keys exceeds the capacity from this operation, evict the least recently used key.
+The functions get and put must each run in O(1) average time complexity.
+
+  #include <unordered_map>
+using namespace std;
+
+class LRUCache {
+private:
+    struct Node {
+        int key, value;
+        Node* prev;
+        Node* next;
+        Node(int k, int v): key(k), value(v), prev(nullptr), next(nullptr) {}
+    };
+
+    int capacity;
+    unordered_map<int, Node*> cache;
+    Node* head;
+    Node* tail;
+
+    // Add node right after head
+    void addToFront(Node* node) {
+        node->next = head->next;
+        node->prev = head;
+        head->next->prev = node;
+        head->next = node;
+    }
+
+    // Remove node from list
+    void removeNode(Node* node) {
+        Node* prevNode = node->prev;
+        Node* nextNode = node->next;
+        prevNode->next = nextNode;
+        nextNode->prev = prevNode;
+    }
+
+    // Move existing node to front (most recently used)
+    void moveToFront(Node* node) {
+        removeNode(node);
+        addToFront(node);
+    }
+
+public:
+    LRUCache(int capacity) {
+        this->capacity = capacity;
+
+        // Create dummy head and tail
+        head = new Node(0, 0);
+        tail = new Node(0, 0);
+        head->next = tail;
+        tail->prev = head;
+    }
+
+    int get(int key) {
+        if (cache.find(key) != cache.end()) {
+            Node* node = cache[key];
+            moveToFront(node);
+            return node->value;
+        }
+        return -1;
+    }
+
+    void put(int key, int value) {
+        if (cache.find(key) != cache.end()) {
+            Node* node = cache[key];
+            node->value = value;
+            moveToFront(node);
+        } else {
+            if (cache.size() == capacity) {
+                Node* lru = tail->prev;
+                removeNode(lru);
+                cache.erase(lru->key);
+                delete lru;
+            }
+            Node* newNode = new Node(key, value);
+            cache[key] = newNode;
+            addToFront(newNode);
+        }
+    }
+
+    ~LRUCache() {
+        Node* curr = head;
+        while (curr) {
+            Node* next = curr->next;
+            delete curr;
+            curr = next;
+        }
+    }
+};
